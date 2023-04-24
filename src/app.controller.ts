56ac5f0e447a19ab412a254cb6d2df406d370dc8
David Fernando M <david.mesa1@misena.edu.co>
2023-04-24 14:14:51 -0500

    fix: creations of controllers

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -14,31 +14,4 @@ export class AppController {
   newEndpoint() {
     return 'Yo soy nuevo';
   }
-
-  @Get('/products/filter')
-  getProductFilter() {
-    return 'Yo soy un filtro';
-  }
-
-  @Get('/products/:productId')
-  getProduct(@Param('productId') productId: string) {
-    return `Product ${productId}`;
-  }
-
-  @Get('/products/')
-  getProducts(
-    @Query('limit') limit = 100,
-    @Query('offset') offset = 0,
-    @Query('brand') brand: string,
-  ) {
-    return `products: limit=> ${limit} offset=> ${offset} brand=> ${brand}`;
-  }
-
-  @Get('/categories/:categoryId/products/:productId')
-  getCategory(
-    @Param('categoryId') categoryId: string,
-    @Param('productId') productId: string,
-  ) {
-    return `Product ${productId} AND ${categoryId}`;
-  }
 }
